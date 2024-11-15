711405c1d2157ba7ac810f90f918ac641d24e98d
ALICJA\alicj <alicjapiotrowicz123@gmail.com>
2024-11-15 12:51:06 +0100

    cursor's animation

--- AddCharTotext/main.js
+++ AddCharTotext/main.js
@@ -1,12 +1,21 @@
 const spanText = document.querySelector(".text");
+const spnCursor = document.querySelector(".cursor");
 const txt =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce lobortis dignissim lacus, eu convallis elit dictum vitae. Pellentesque convallis ornare ultricies. Quisque volutpat varius libero, nec finibus augue lobortis et. Cras condimentum augue sed ligula interdum tempus. Donec a libero lacus. Fusce dignissim, lacus eu pulvinar ullamcorper, velit felis pharetra elit, ut efficitur eros ex non ex.";
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce lobortis dignissim lacus, eu convallis elit dictum vitae. Pellentesque convallis ornare ultricies. Quisque volutpat varius libero, nec finibus augue lobortis et.";
 
 let indexText = 0;
 const time = 40;
+
 const addLetter = () => {
   spanText.textContent += txt[indexText];
   indexText++;
   if (indexText === txt.length) clearInterval(indexTyping);
 };
+
+const cursorAnimation = () => {
+  spnCursor.classList.toggle("active");
+  console.log("ok");
+};
+
 const indexTyping = setInterval(addLetter, time);
+setInterval(cursorAnimation, 400);
